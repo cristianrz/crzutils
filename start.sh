bd1791e4e08defd5b0d0f809fd9d146c1d68ed56
Cristian Ariza <dev@cristianrz.com>
2020-02-10 21:33:37 +0000

    Throw an error if command is not found

--- start.sh
+++ start.sh
@@ -25,4 +25,9 @@ if test "$1" = "--help"; then
 	exit
 fi
 
-exec nohup "$@" > /dev/null 2>&1 &
+if ! command -v "$1" > /dev/null 2>&1; then
+	printf "start: no such command '%s'" "$1" >&2
+	exit 1
+fi
+
+nohup "$@" > /dev/null 2>&1 &
