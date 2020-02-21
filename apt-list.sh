1ef0c3f12422000f3c223b0253efec11a03349d7
Cristian Ariza <dev@cristianrz.com>
2020-02-21 17:17:15 +0000

    Merge apt-du and apt-list

--- apt-list.sh
+++ apt-list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Shows installed packages
+# Shows installed packages sorted by size
 #
 # Copyright (C) 2020 Cristian Ariza
 #
@@ -13,13 +13,20 @@ Usage: $(basename "$0") [OPTION]
 
 Shows installed packages.
 
-	--help    display this help and exit"
+	-h  display this help and exit
+	-d  print disk usage"
 
-case "$1" in
-"--help")
-	printf '%s\n' "$usage"
-	exit
-	;;
-esac
+while getopts "hd" c; do
+	case "$c" in
+	d)
+		dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n
+		exit 0
+		;;
+	*)
+		printf '%s\n' "$usage"
+		exit 1
+		;;
+	esac
+done
 
 dpkg --get-selections | grep -v deinstall | cut -f1 | cut -d':' -f1 | sort
